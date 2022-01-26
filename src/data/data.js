7a4f34f8df72cccec2502f78283807359defe577
BabbaRegina <irene.nicolini@gmail.com>
2022-01-26 20:26:47 +0100

    fix(api): use next-connect to add basic crud ops

---
+++ src/data/data.js
@@ -0,0 +1,6 @@
+const notes = [{
+    id: 1643224815938,
+    text: 'note sample'
+}]
+
+module.exports = notes
--- src/pages/api/index.js
+++
@@ -1,5 +0,0 @@
-export default (req, res) => {
-    res.statusConde = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ message: 'Hello there, I\'m your API' }))
-}
